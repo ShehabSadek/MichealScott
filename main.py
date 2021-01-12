db14f77cc675d6e05a566fc09339ed2e6c5cf5b1
Shehab <shehab.m.sadek@gmail.com>
2021-01-12 00:37:16 +0000

    cleanup

--- main.py
+++ main.py
@@ -32,74 +32,77 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+  
+  if message.author == client.user:
+    return
 
-    msg = message.content
+  msg = message.content
 
-    if msg.lower().startswith('?micheal'):
-        quote = get_quote()
-        await message.channel.send(quote)
+  if msg.lower().startswith('?micheal') or msg.lower().startswith('?mike'):
+    quote = get_quote()
+    await message.channel.send(quote)
 
-    elif msg.lower().startswith('?mike'):
-        quote = get_quote()
-        await message.channel.send(quote)
+  elif msg.lower().startswith('?ammar') or msg.lower().startswith('?why'):
+    await message.channel.purge(limit=1)
+    await message.channel.send("Why are you the way that you are?")
+    time.sleep(1.5)
+    await message.channel.send(
+    "Honestly, every time I try to do something fun or exciting, you make it not that way"
+    )
+    time.sleep(2.5)
+    await message.channel.send(
+    " I hate so much about the things that you choose to be")
 
-    elif msg.lower().startswith('?ammar') or msg.lower().startswith('?why'):
-        await message.channel.purge(limit=1)
-        await message.channel.send("Why are you the way that you are?")
-        time.sleep(1.5)
-        await message.channel.send(
-            "Honestly, every time I try to do something fun or exciting, you make it not that way"
-        )
-        time.sleep(2.5)
-        await message.channel.send(
-            " I hate so much about the things that you choose to be")
-
-    elif any(word in msg for word in keyword):
-        await message.channel.send(random.choice(starter_mike))
-        await message.channel.send(random.choice(["https://tenor.com/view/michael-scott-prison-mike-kissy-prison-mike-kissy-gif-13582037","https://media.giphy.com/media/niC0LL8nmXnWp0d7Sn/giphy.gif"]))
+  elif any(word in msg for word in keyword):
+    await message.channel.send(random.choice(starter_mike))
+    await message.channel.send(random.choice["https://tenor.com/view/michael-scott-prison-mike-kissy-prison-mike-kissy-gif-13582037","https://media.giphy.com/media/niC0LL8nmXnWp0d7Sn/giphy.gif"])
 
   
-    elif msg.lower().startswith('?she'):
-       await message.channel.purge(limit=1)
-       await message.channel.send("That's what she said ")
+  elif msg.lower().startswith('?she'):
+    await message.channel.purge(limit=1)
+    await message.channel.send("That's what she said ")
 
-    elif msg.lower().startswith('cringe'):
-      await message.channel.purge(limit=1)
-      await message.channel.send("https://tenor.com/view/shit-cringe-gif-9261764")
-    elif msg.lower().startswith('kms'):
-      await message.channel.purge(limit=1)
-      await message.channel.send("https://tenor.com/view/wanna-kill-myself-the-office-michael-scott-steve-carell-frustrated-gif-13414030")
-    elif msg.lower().startswith('kill you'):
-      await message.channel.purge(limit=1)
-      await message.channel.send("https://tenor.com/view/embarrassed-michaelscott-theoffice-gif-5120730")
-    elif msg.lower().startswith('noo'):
-      await message.channel.purge(limit=1)
-      await message.channel.send("https://tenor.com/view/no-do-not-want-the-office-michael-scott-steve-carrell-gif-5644457")
-    elif msg.lower().startswith('vance'):
-      await message.channel.purge(limit=1)
-      await message.channel.send("https://tenor.com/view/bob-vance-vance-refrigeration-the-office-shake-hands-introduce-gif-17948038")
-    elif msg.lower().startswith('huh'):
-      await message.channel.purge(limit=1)
-      await message.channel.send(random.choice(["https://media.giphy.com/media/5wWf7H89PisM6An8UAU/giphy.gif","https://media.giphy.com/media/SAAMcPRfQpgyI/giphy.gif"]))
-    elif msg.lower().startswith('white'):
-      await message.channel.purge(limit=1)
-      await message.channel.send("https://media.giphy.com/media/RLHP0SjHQQjIc/giphy.gif")
-    elif msg.lower().startswith('broke'):
-     await message.channel.purge(limit=1)
-     await message.channel.send("https://media.giphy.com/media/NbsN8ERSfcCys/giphy.gif")
+  elif msg.lower().startswith('cringe'):
+    await message.channel.purge(limit=1)
+    await message.channel.send("https://tenor.com/view/shit-cringe-gif-9261764")
+
+  elif msg.lower().startswith('kms'):
+    await message.channel.purge(limit=1)
+    await message.channel.send("https://tenor.com/view/wanna-kill-myself-the-office-michael-scott-steve-carell-frustrated-gif-13414030")
+
+  elif msg.lower().startswith('kill you'):
+    await message.channel.purge(limit=1)
+    await message.channel.sen("https://tenor.com/view/embarrassed-michaelscott-theoffice-gif-5120730")
+
+  elif msg.lower().startswith('noo')or msg.lower().startswith('toby'):
+    await message.channel.purge(limit=1)
+    await message.channel.send("https://tenor.com/view/no-do-not-want-the-office-michael-scott-steve-carrell-gif-5644457")
+
+  elif msg.lower().startswith('vance'):
+    await message.channel.purge(limit=1)
+    await message.channel.send("https://tenor.com/view/bob-vance-vance-refrigeration-the-office-shake-hands-introduce-gif-17948038")
+
+  elif msg.lower().startswith('huh'):
+    await message.channel.purge(limit=1)
+    await message.channel.send(random.choice(["https://media.giphy.com/media/5wWf7H89PisM6An8UAU/giphy.gif","https://media.giphy.com/media/SAAMcPRfQpgyI/giphy.gif"]))
+
+  elif msg.lower().startswith('white'):
+    await message.channel.purge(limit=1)
+    await message.channel.send("https://media.giphy.com/media/RLHP0SjHQQjIc/giphy.gif")
+    
+  elif msg.lower().startswith('broke'):
+    await message.channel.purge(limit=1)
+    await message.channel.send("https://media.giphy.com/media/NbsN8ERSfcCys/giphy.gif")
+
+  elif message.content.startswith('?help'):
+    embedVar = discord.Embed(title="Random Micheal quotes", description="?micheal or ?mike", color=0x00ffff)
+    embedVar.add_field(name="Famous lines", value="?why, ?she", inline=False)
+    embedVar.add_field(name="Prison mike keywords", value="prison, jail, prison food, prison mike", inline=False)
+    embedVar.add_field(name="Gifs keywords", value="cringe, kms, kill you, noo, vance, huh, white, broke", inline=False)
+    embedVar.add_field(name="----------------------------------",value="Nothing is case sensitive , keywords & Gifs dont need prefix",inline=True)
+    await message.channel.send(embed=embedVar)
 
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('?help'):
-        embedVar = discord.Embed(title="Random Micheal quotes", description="?micheal or ?mike", color=0x00ffff)
-        embedVar.add_field(name="Famous lines", value="?why, ?she", inline=False)
-        embedVar.add_field(name="Prison mike keywords", value="prison, jail, prison food, prison mike", inline=False)
-        embedVar.add_field(name="Gifs keywords", value="ringe, kms, kill you, noo, vance, huh, white, broke", inline=False)
-        embedVar.add_field(name="----------------------------------",value="Nothing is case sensitive , keywords & Gifs dont need prefix",inline=True)
-        await message.channel.send(embed=embedVar)
+
 keep_alive()
 client.run(os.getenv('KeY'))
-
